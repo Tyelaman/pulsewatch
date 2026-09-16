507a87bda46bc1d022b0a5f617b33d611858ac94
Tyelaman <tyelama@purdue.edu>
2026-09-15 23:25:13 -0400

    Added new package for api checking

---
+++ src/main/java/com/tyelaman/pulsewatch/demo/DemoServiceController.java
@@ -0,0 +1,37 @@
+package com.tyelaman.pulsewatch.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoServiceController {
+
+    private boolean healthy = true;
+
+    @GetMapping("/target")
+    public ResponseEntity<String> getTarget() {
+        if (healthy) {
+            return ResponseEntity.ok("Demo service is healthy");
+        }
+
+        return ResponseEntity
+                .internalServerError()
+                .body("Demo service is failing");
+    }
+
+    @PostMapping("/fail")
+    public String fail() {
+        healthy = false;
+        return "Demo service is now failing";
+    }
+
+    @PostMapping("/recover")
+    public String recover() {
+        healthy = true;
+        return "Demo service has recovered";
+    }
+}
